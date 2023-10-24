77bf75cd669262bb3eb646b3378b643347758a84
ogzhanolguncu <ogzhan11@gmail.com>
2023-10-24 14:00:27 +0300

    Implement design of the missing data  section

--- packages/react-databrowser/src/components/databrowser/components/data-display-container/index.tsx
+++ packages/react-databrowser/src/components/databrowser/components/data-display-container/index.tsx
@@ -14,7 +14,7 @@ export const DataDisplayContainer = ({ selectedDataKeyTypePair, onDataKeyChange 
   }
 
   return (
-    <div className="col-span-4 lg:border-l">
+    <div className="col-span-4 border-l">
       <div className="h-full px-4 py-6 lg:px-8">
         <div className="h-full space-y-6">
           <DataDisplayHeader selectedDataKey={selectedDataKeyTypePair[0]} onDataKeyChange={onDataKeyChange} />
