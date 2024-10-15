70a65d9c29559a8be4f328765785c14f0444b59d
ytkimirti <yusuftaha9@gmail.com>
2024-10-15 16:01:46 +0300

    WIP

--- packages/react-databrowser/src/components/databrowser/components/display/list-display.tsx
+++ packages/react-databrowser/src/components/databrowser/components/display/list-display.tsx
@@ -1,5 +1,9 @@
 import { DataType } from "@/types";
+import { DisplayHeader } from "./display-header";
 
 export const ListDisplay = ({ dataKey, type }: { dataKey: string; type: DataType }) => {
-  return <>{"<ListDisplay /> (WIP)"}</>;
+  return <div>
+    <DisplayHeader dataKey={dataKey} type={type} />
+    <div></div>
+  </div>
 };
