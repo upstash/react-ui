acf3827341fbf39d72d137b364f358803fd10360
ytkimirti <yusuftaha9@gmail.com>
2024-10-30 09:11:55 +0300

    fix tsup config

--- packages/react-databrowser/tsup.config.ts
+++ packages/react-databrowser/tsup.config.ts
@@ -4,9 +4,6 @@ export default defineConfig({
   entry: ["src/index.ts"],
   format: ["cjs", "esm"],
   splitting: true,
-  sourcemap: false,
   clean: true,
   dts: true,
-  minify: true,
-  minifyWhitespace: true,
 })
