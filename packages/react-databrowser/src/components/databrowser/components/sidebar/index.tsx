6f76cab8c5f6087a40c00c81e6c7c4dc9e792015
ytkimirti <yusuftaha9@gmail.com>
2024-11-07 20:01:59 +0300

    cleanup

--- packages/react-databrowser/src/components/databrowser/components/sidebar/index.tsx
+++ packages/react-databrowser/src/components/databrowser/components/sidebar/index.tsx
@@ -1,15 +1,10 @@
-import { useDatabrowserStore } from "@/store"
-import { IconX } from "@tabler/icons-react"
-
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-
 import { useKeys } from "../../hooks/use-keys"
 import { AddKeyModal } from "../add-key-modal"
 import { DisplayDbSize } from "./db-size"
 import { Empty } from "./empty"
 import { InfiniteScroll } from "./infinite-scroll"
 import { KeysList } from "./keys-list"
+import { SearchInput } from "./search-input"
 import { LoadingSkeleton } from "./skeleton-buttons"
 import { DataTypeSelector } from "./type-selector"
 
@@ -33,7 +28,7 @@ export function Sidebar() {
           <DataTypeSelector />
 
           {/* Search */}
-          <KeySearch />
+          <SearchInput />
         </div>
       </div>
 
@@ -45,35 +40,6 @@ export function Sidebar() {
         </InfiniteScroll>
       ) : (
         <Empty />
-      )}
-    </div>
-  )
-}
-
-const KeySearch = () => {
-  const { setSearchKey, search } = useDatabrowserStore()
-
-  return (
-    <div className="relative grow">
-      <Input
-        placeholder="Search"
-        className={"rounded-l-none border-zinc-300 font-normal"}
-        onChange={(e) => setSearchKey(e.target.value)}
-        value={search.key}
-      />
-      {search.key && (
-        <Button
-          type="button"
-          variant="link"
-          size="icon"
-          className="absolute right-1 top-1/2 h-5 w-5 -translate-y-1/2 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-100"
-          onClick={() => {
-            setSearchKey("")
-          }}
-        >
-          <IconX size={16} />
-          <span className="sr-only">Clear</span>
-        </Button>
       )}
     </div>
   )
