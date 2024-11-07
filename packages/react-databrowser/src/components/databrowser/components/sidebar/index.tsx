f7587f950a08a4806083ec7944348392c12a94a3
ytkimirti <yusuftaha9@gmail.com>
2024-11-07 20:00:52 +0300

    add clear button to search

--- packages/react-databrowser/src/components/databrowser/components/sidebar/index.tsx
+++ packages/react-databrowser/src/components/databrowser/components/sidebar/index.tsx
@@ -1,5 +1,7 @@
 import { useDatabrowserStore } from "@/store"
+import { IconX } from "@tabler/icons-react"
 
+import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 
 import { useKeys } from "../../hooks/use-keys"
@@ -13,7 +15,6 @@ import { DataTypeSelector } from "./type-selector"
 
 export function Sidebar() {
   const { keys, query } = useKeys()
-  const { setSearchKey, search } = useDatabrowserStore()
 
   return (
     <div className="flex h-full flex-col gap-2 rounded-xl border p-1">
@@ -32,13 +33,7 @@ export function Sidebar() {
           <DataTypeSelector />
 
           {/* Search */}
-          <Input
-            type="text"
-            placeholder="Search"
-            className={"rounded-l-none border-zinc-300 font-normal"}
-            onChange={(e) => setSearchKey(e.target.value)}
-            value={search.key}
-          />
+          <KeySearch />
         </div>
       </div>
 
@@ -50,6 +45,35 @@ export function Sidebar() {
         </InfiniteScroll>
       ) : (
         <Empty />
+      )}
+    </div>
+  )
+}
+
+const KeySearch = () => {
+  const { setSearchKey, search } = useDatabrowserStore()
+
+  return (
+    <div className="relative grow">
+      <Input
+        placeholder="Search"
+        className={"rounded-l-none border-zinc-300 font-normal"}
+        onChange={(e) => setSearchKey(e.target.value)}
+        value={search.key}
+      />
+      {search.key && (
+        <Button
+          type="button"
+          variant="link"
+          size="icon"
+          className="absolute right-1 top-1/2 h-5 w-5 -translate-y-1/2 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-100"
+          onClick={() => {
+            setSearchKey("")
+          }}
+        >
+          <IconX size={16} />
+          <span className="sr-only">Clear</span>
+        </Button>
       )}
     </div>
   )
